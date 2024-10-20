07377c498c5c378b70b2cbe23453de03799c351c
hungdev123 <hungdev1405@gmail.com>
2024-10-20 10:03:51 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.static('public')); // Thư mục chứa file HTML và JS
+
+// Đăng ký người dùng
+app.post('/register', (req, res) => {
+    const newUser = req.body;
+    
+    fs.readFile('users.json', (err, data) => {
+        if (err) return res.status(500).send(err);
+        
+        const users = JSON.parse(data);
+        users.push(newUser);
+        
+        fs.writeFile('users.json', JSON.stringify(users), (err) => {
+            if (err) return res.status(500).send(err);
+            res.status(201).send('User registered successfully!');
+        });
+    });
+});
+
+// Đăng nhập người dùng
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    fs.readFile('users.json', (err, data) => {
+        if (err) return res.status(500).send(err);
+        
+        const users = JSON.parse(data);
+        const user = users.find(u => u.username === username && u.password === password);
+        
+        if (user) {
+            res.status(200).send('Login successful!');
+        } else {
+            res.status(401).send('Invalid username or password.');
+        }
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
